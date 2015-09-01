7c1bc723fa5902d2254cc82ec1c7dc66ea4d8477
yasuhisa1984 <penmouse275@icloud.com>
2015-09-01 07:54:13 +0000

    ad top page

--- config/application.rb
+++ config/application.rb
@@ -8,17 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Microposts
   class Application < Rails::Application
-    
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
-    end
-  end
-end
-
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -33,5 +22,11 @@ end
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+     config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
