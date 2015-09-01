55dbb6b21e0bcc17522b512ea89658b0b58eec1f
yasuhisa1984 <penmouse275@icloud.com>
2015-09-01 06:40:07 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,17 @@ Bundler.require(*Rails.groups)
 
 module Microposts
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
+
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
