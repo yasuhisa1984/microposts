e94ded9469b97456582017074760fa94c84d039d
yasuhisa1984 <penmouse275@icloud.com>
2015-09-03 09:54:56 +0000

    add sign sign up and user detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase } #コールバック downcaseは小文字に変える
+    validates :name, presence: true, length: { maximum: 50 } #nameは空でなく、また、最大50文字
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },#format正規表現パターンに一致しているか
+                      uniqueness: { case_sensitive: false} #ase_sensitive大文字と小文字を区別するか
+    has_secure_password #認証機能を追加することができる
 end
