d45bef6fbb3df184c06730cc53d5eefcd31a8cb7
yasuhisa1984 <penmouse275@icloud.com>
2015-09-15 02:42:38 +0000

    add home view's timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-    before_save { self.email = email.downcase } #コールバック downcaseは小文字に変える
-    validates :name, presence: true, length: { maximum: 50 } #nameは空でなく、また、最大50文字
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
+  before_save { self.email = email.downcase } #コールバック downcaseは小文字に変える
+  validates :name, presence: true, length: { maximum: 50 } #nameは空でなく、また、最大50文字
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },#format正規表現パターンに一致しているか
                       uniqueness: { case_sensitive: false} #ase_sensitive大文字と小文字を区別するか
-    has_secure_password #認証機能を追加することができる
+  has_secure_password #認証機能を追加することができる
     
   has_many :microposts
 
@@ -36,4 +36,9 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+  # 自分とフォローしているユーザーのつぶやきを取得するメソッド
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
+  end
 end
+
